43a8865e86a70e9b825e1492e9c8480ac9e5346a
Sebastian <sebastian.luque.pl@gmail.com>
2019-06-30 15:00:36 -0500

    final tweaks

--- app/src/main/java/com/cibertec/amplyfm/utils/Constants.java
+++ app/src/main/java/com/cibertec/amplyfm/utils/Constants.java
@@ -2,17 +2,11 @@ package com.cibertec.amplyfm.utils;
 
 
 public class Constants {
-    /*API KEY LAST FM*/
-    public static final String API_KEY = "269773a98Sae72a752cbbe2fba8d8c0a6";
-    /*-------------*/
+    public static final String API_KEY = "269773a98ae72a752cbbe2fba8d8c0a6";
     public static String BASE_URL = "http://ws.audioscrobbler.com/2.0/";
     public static final int TOP_ITEMS_LIMIT = 10;
-    /*API KEY  YOUTUBE*/
     public static final String API_KEY_YT = "AIzaSyCOz2aMoal76CRV6sCFRZ4FxY0RWw7Cd6E";
-    /*---------------*/
-    /*API KEY BING - BUSQUEDA DE IMAGENES*/
     public static final String API_KEY_BING = "88eee31617ad4e3083758eefeb7c626b";
-    /*-----------------------------------*/
     public static String BASE_URL_YT = "https://www.googleapis.com/youtube/v3/search/";
     public static String BASE_URL_BING = "https://api.cognitive.microsoft.com/bing/v7.0/images/search/";
 
