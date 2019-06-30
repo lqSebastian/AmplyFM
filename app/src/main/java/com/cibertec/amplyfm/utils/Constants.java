6f4e347b4ffc0a91f946477773857ee967bc3143
Sebastian <sebastian.luque.pl@gmail.com>
2019-06-30 18:11:28 -0500

    updated yt api key

--- app/src/main/java/com/cibertec/amplyfm/utils/Constants.java
+++ app/src/main/java/com/cibertec/amplyfm/utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String API_KEY = "269773a98ae72a752cbbe2fba8d8c0a6";
     public static String BASE_URL = "http://ws.audioscrobbler.com/2.0/";
     public static final int TOP_ITEMS_LIMIT = 10;
-    public static final String API_KEY_YT = "AIzaSyCOz2aMoal76CRV6sCFRZ4FxY0RWw7Cd6E";
+    public static final String API_KEY_YT = "AIzaSyBbU9oRxrdYm2f_y3B38CuwG0RsrIuBt18";
     public static final String API_KEY_BING = "88eee31617ad4e3083758eefeb7c626b";
     public static String BASE_URL_YT = "https://www.googleapis.com/youtube/v3/search/";
     public static String BASE_URL_BING = "https://api.cognitive.microsoft.com/bing/v7.0/images/search/";
